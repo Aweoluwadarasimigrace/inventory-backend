06ddbdc2cf5617092ff2163f7504a53d879dc801
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-07-13 00:12:58 +0100

    new commit

--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -1,36 +1,41 @@
 const { default: mongoose } = require("mongoose");
 
-
 const authSchema = new mongoose.Schema({
-    companyName: {
-        type: String,
-        required: true
-    },
-    contact:{
-        type:Number,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+  companyName: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+  countrycode: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    country:{
-        type:String,
-        required: true
-    },
-    verified: {
-        type: Boolean,
-        default: false
-    },
-})
+const Auth = mongoose.model("auth", authSchema);
 
-
-const Auth = mongoose.model("auth", authSchema)
-
-module.exports = Auth
+module.exports = Auth;
