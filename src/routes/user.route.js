7214bb6dd0ed64e566c904f3c7b5ad785a792742
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-07-19 20:21:33 +0100

    newfilrs

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { createUserByAdmin } = require("../controller/user.controller")
+const { createUserByAdmin, getUserByAdmin } = require("../controller/user.controller")
 const { verifyToken, verifyIsAdmin } = require("../middleware/verifytoken.middleware")
 const upload = require("../middleware/uploads.middleware")
 const router = express.Router()
