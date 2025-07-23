2843975f7fd0f6e9110069f5ab0871bf8c19afc2
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-07-23 10:22:25 +0100

    new commit

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -241,7 +241,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(201).json({
@@ -260,7 +260,7 @@ const logOut = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "none",
     });
 
     return res.json({ message: "logout successful" });
