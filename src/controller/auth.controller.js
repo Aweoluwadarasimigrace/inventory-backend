0874c6bd2864b287f8da167563d30b60c73a2551
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-08-18 21:52:18 +0100

    new files

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -136,7 +136,7 @@ const verifyEmail = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json({ message: "Email verified successfully!" });
